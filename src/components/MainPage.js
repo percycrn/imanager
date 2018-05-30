15454a53b2d6720b0f69ee4b2bde91cfe1b601f1
bin li <djfos@outlook.com>
2018-05-30 19:58:21 +0800

    done:before merge next:

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,87 +1,41 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import { Route, Switch ,BrowserHistory,NavLink} from "react-router-dom";
-import Activity from "./Activity";
-import Account from "./Account";
-import Application from "./Application";
-import { Menu, Dropdown, Icon } from "antd";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Route, Switch, BrowserHistory, NavLink } from 'react-router-dom';
+import Activity from './Activity';
+import Account from './Account';
+import Application from './Application';
+import { Dropdown, Icon } from 'antd';
+import  _Header  from './Header';
 
 class MainPage extends Component {
-  
   render() {
-    const menu = (
-      // header
-      <Menu>
-        <Menu.Item>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="https://www.baidu.com/"
-          >
-            profile
-          </a>
-        </Menu.Item>
-        <Menu.Item>
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href="http://www.qq.com/"
-          >
-            logout
-          </a>
-        </Menu.Item>
-      </Menu>
-    );
-    // mainpage导航栏
-    const SubMenu = Menu.SubMenu;
     return (
       <div>
-        <Header>
-          <Dropdown overlay={menu}>
-            <a className="ant-dropdown-link" href="#">
-              Welcome to activity manager! <Icon type="down" />
-            </a>
-          </Dropdown>
-        </Header>
-        
+        <Header />
         <SideBar>
           <div>logo</div>
-          <NavLink
-            className="App-sidebaritem"
-            to="/activity"
-          >
+          <NavLink className="App-sidebaritem" to="/activity">
             activity
           </NavLink>
-          <NavLink
-            className="App-sidebaritem"
-            to="/application"
-          >
+          <NavLink className="App-sidebaritem" to="/application">
             application
           </NavLink>
-          <NavLink
-            className="App-sidebaritem"
-            to="/account"
-          >
+          <NavLink className="App-sidebaritem" to="/account">
             account
           </NavLink>
         </SideBar>
-        <Switch>
-          <Route path="/activity" component={Activity} Activity />
-          <Route path="/account" component={Account} Account />
-          <Route path="/application" component={Application} Application />
-        </Switch>
+
+        <Route path="/activity" component={Activity} Activity />
+        <Route path="/account" component={Account} Account />
+        <Route path="/application" component={Application} Application />
       </div>
     );
-  }
-
-  jump(path) {
-   window.history.pushState({},"",path)
   }
 }
 
 export default MainPage;
 
-const Header = styled.div`
+const Header = styled(_Header)`
   max-width: 1290px;
   margin: 0px auto;
   padding: 16px 30px 0px 30px;
@@ -102,7 +56,4 @@ const SideBar = styled.div`
   background: transparent linear-gradient(80deg, #0b4182 1%, #1a78cf 99%) repeat
     scroll 0% 0%;
   z-index: 10;
-`;
-const Display = styled(Switch)`
-  grid-area: route-display;
 `;
