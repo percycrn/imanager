55cd4d8358ab6aa420a3c90ef2fb28928860fbd2
eric <853879506@qq.com>
2018-05-30 19:45:20 +0800

    分离css

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
-import styled from "styled-components";
-import { Route, Switch ,BrowserHistory,NavLink} from "react-router-dom";
+import { Route, Switch, NavLink } from "react-router-dom";
 import Activity from "./Activity";
 import Account from "./Account";
 import Application from "./Application";
 import { Menu, Dropdown, Icon } from "antd";
 
 class MainPage extends Component {
-  
   render() {
     const menu = (
       // header
@@ -33,38 +31,28 @@ class MainPage extends Component {
       </Menu>
     );
     // mainpage导航栏
-    const SubMenu = Menu.SubMenu;
     return (
       <div>
-        <Header>
+        <div className="App-header">
           <Dropdown overlay={menu}>
-            <a className="ant-dropdown-link" href="#">
+            <a className="ant-dropdown-link">
               Welcome to activity manager! <Icon type="down" />
             </a>
           </Dropdown>
-        </Header>
-        
-        <SideBar>
+        </div>
+
+        <div className="App-sidebar">
           <div>logo</div>
-          <NavLink
-            className="App-sidebaritem"
-            to="/activity"
-          >
+          <NavLink className="App-sidebaritem" to="/activity">
             activity
           </NavLink>
-          <NavLink
-            className="App-sidebaritem"
-            to="/application"
-          >
+          <NavLink className="App-sidebaritem" to="/application">
             application
           </NavLink>
-          <NavLink
-            className="App-sidebaritem"
-            to="/account"
-          >
+          <NavLink className="App-sidebaritem" to="/account">
             account
           </NavLink>
-        </SideBar>
+        </div>
         <Switch>
           <Route path="/activity" component={Activity} Activity />
           <Route path="/account" component={Account} Account />
@@ -75,34 +63,9 @@ class MainPage extends Component {
   }
 
   jump(path) {
-   window.history.pushState({},"",path)
+    window.history.pushState({}, "", path);
   }
 }
 
 export default MainPage;
 
-const Header = styled.div`
-  max-width: 1290px;
-  margin: 0px auto;
-  padding: 16px 30px 0px 30px;
-  text-align: right;
-  position: relative;
-  white-space: nowrap;
-`;
-
-const SideBar = styled.div`
-  display: flex;
-  flex-flow: column;
-  position: fixed;
-  top: 0px;
-  left: 0px;
-  height: 100%;
-  width: 110px;
-  text-align: center;
-  background: transparent linear-gradient(80deg, #0b4182 1%, #1a78cf 99%) repeat
-    scroll 0% 0%;
-  z-index: 10;
-`;
-const Display = styled(Switch)`
-  grid-area: route-display;
-`;
